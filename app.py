8e00e57f5f81b519742dd5b40dd0e479fbc6a73b
Yure Santana <yuredosanjossantana@gmail.com>
2025-09-29 17:05:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,79 +35,80 @@ st.markdown(
 
 col1, col2, col3 = st.columns(3)
 with col1:
-st.markdown(
-    f"""
-    <div style="
-        width:165px;
-        height:120px;
-        background-color:#D0FFCC;
-        border-radius:4px;
-	margin-left:0px;
-        margin-right:auto; 
-	margin-top:40px;
- 	display:flex;
-        align-items:flex-start;
-        justify-content:center;  
-	color:#D00003
-	font-size:24px;
-	font-family:Inria Serif;
-        text-align:center;
-        ">
-	Temperatura:<br><br>
-	{temperatura} °C
-    </div>
-    """,
-    unsafe_allow_html=True
+	st.markdown(
+    	f"""
+    	<div style="
+        	width:165px;
+        	height:120px;
+        	background-color:#D0FFCC;
+       		border-radius:4px;
+		margin-left:10px;
+        	margin-right:auto; 
+		margin-top:40px;
+ 		display:flex;
+        	align-items:flex-start;
+        	justify-content:center;  
+		color:#D00003
+		font-size:24px;
+		font-family:Inria Serif;
+        	text-align:center;
+        	">
+		Humidade do ar:<br><br>
+		{humidade} %
+    	</div>
+    	""",
+   	unsafe_allow_html=True
 )
 with col2:
-st.markdown(
-    f"""
-    <div style="
-        width:165px;
-        height:120px;
-        background-color:#D0FFCC;
-        border-radius:4px;
-	margin-left:10;
-        margin-right:auto; 
-	margin-top:40px;
- 	display:flex;
-        align-items:flex-start;
-        justify-content:center;  
-	color:#D00003
-	font-size:24px;
-	font-family:Inria Serif;
-        text-align:center;
-        ">
-	Vel. do Vento:<br><br>
-	{vento} km/h
-    </div>
-    """,
-    unsafe_allow_html=True
+	st.markdown(
+    	f"""
+    	<div style="
+        	width:165px;
+        	height:120px;
+        	background-color:#D0FFCC;
+       		border-radius:4px;
+		margin-left:10px;
+        	margin-right:auto; 
+		margin-top:40px;
+ 		display:flex;
+        	align-items:flex-start;
+        	justify-content:center;  
+		color:#D00003
+		font-size:24px;
+		font-family:Inria Serif;
+        	text-align:center;
+        	">
+		Humidade do ar:<br><br>
+		{humidade} %
+    	</div>
+    	""",
+   	unsafe_allow_html=True
 )
+
 with col3:
-st.markdown(
-    f"""
-    <div style="
-        width:165px;
-        height:120px;
-        background-color:#D0FFCC;
-        border-radius:4px;
-	margin-left:10px;
-        margin-right:auto; 
-	margin-top:40px;
- 	display:flex;
-        align-items:flex-start;
-        justify-content:center;  
-	color:#D00003
-	font-size:24px;
-	font-family:Inria Serif;
-        text-align:center;
-        ">
-	Humidade do ar:<br><br>
-	{humidade} %
-    </div>
-    """,
-    unsafe_allow_html=True
+	st.markdown(
+    	f"""
+    	<div style="
+        	width:165px;
+        	height:120px;
+        	background-color:#D0FFCC;
+       		border-radius:4px;
+		margin-left:10px;
+        	margin-right:auto; 
+		margin-top:40px;
+ 		display:flex;
+        	align-items:flex-start;
+        	justify-content:center;  
+		color:#D00003
+		font-size:24px;
+		font-family:Inria Serif;
+        	text-align:center;
+        	">
+		Humidade do ar:<br><br>
+		{humidade} %
+    	</div>
+    	""",
+   	unsafe_allow_html=True
 )
 
 
