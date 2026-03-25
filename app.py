c7684978176d618da74a1867a3b45bdea09d79b2
Yure Santana <yuredosanjossantana@gmail.com>
2026-03-25 17:17:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def get_last_data():
 
                 temperatura = last_data.get("Temperatura", 0)
                 umidade = last_data.get("Umidade", 0)
-		vento = last_data.get("Vento", 0)
-		hora = last_data.get("Hora", "N/A")  # <--- default seguro
-		data = last_data.get("Data", "N/A")  # <--- default seguro
+                vento = last_data.get("Vento", 0)
+                hora = last_data.get("Hora", "N/A")  # <--- default seguro
+                data = last_data.get("Data", "N/A")  # <--- default seguro
                 return temperatura, umidade, vento, hora, data
         else:
             st.error(f"Erro HTTP: {r.status_code}")
